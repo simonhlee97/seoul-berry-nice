e57cdd43d0a540fa2963edd1db73e47ef814c006
Simon Lee <simonhlee97@hotmail.com>
2022-06-16 16:25:27 +0900

    add auth register

--- start/routes.ts
+++ start/routes.ts
@@ -1,32 +1,14 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer''
-|
-*/
-
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get("/", "PagesController.home").as("home");
 Route.get('/about', 'PagesController.about').as('about')
 Route.get("/contact", "PagesController.contact").as('contact')
 
-Route.get("/things", "TodosController.index").as("things");
-Route.post("/things", "TodosController.store")
+// Route.get("/things", "TodosController.index").as("things");
+// Route.post("/things", "TodosController.store")
 
+Route.get("/bites", "BitesController.index")
+Route.post("/bites", "BitesController.store");
 
 Route.get('register', 'AuthController.registerShow').as('auth.register.show')
 Route.post('register', 'AuthController.register').as('auth.register')
